c47f3a69bb03daae80e296e7ff41a45150024199
Hasan Özdişçi <hasanozdisci@gmail.com>
2022-07-23 17:15:33 +0300

    delete button is not disabled after the empty list

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,48 @@
-const txtItem = document.querySelector('#txtItem');
-const addButton = document.querySelector('#btnCreate');
+const txtItem = document.querySelector('#txtItem')
+const btnCreate = document.querySelector('#btnCreate')
 const myList = document.querySelector('#myList')
 
-
-addButton.addEventListener('click', function () {
-    const inputValue = txtItem.value;
-    const li = document.createElement('li');
-    li.classList.add('checked');
-    li.innerHTML = inputValue;
-    li.className = 'list-group-item';
-    myList.append(li);
+// Create List Element
+function createElement() {
+    const newLI = document.createElement('LI');
+    newLI.classList.add('checked')
+    newLI.className = 'list-group-item'
+    newLI.textContent = txtItem.value
+    myList.append(newLI)
     txtItem.value = ""
-})
+}
 
-myList.addEventListener('click', function (item) {
+// when the button clicked createElement function will create another li element
+btnCreate.addEventListener('click', createElement)
+
+
+
+function selectMyListElement(item) {
     if (item.target.tagName == 'LI') {
         item.target.classList.toggle('checked');
         deleteButton();
     }
-})
+}
 
+// myList elements clickable
+myList.addEventListener('click', selectMyListElement)
+
+
+// if myList have any element delete button display none
 function deleteButton() {
     let checkListElement = document.querySelectorAll('.list-group-item.checked')
     if (checkListElement.length > 0) {
         document.querySelector('#deleteAll').classList.remove('d-none');
     } else {
-        document.querySelector('#deleteAll').classList.add('d-none')
+        document.querySelector('#deleteAll').classList.add('d-none');
     }
 }
 
+
+
 const deleteAll = document.querySelector('#deleteAll');
 
+// delete list elements with delete button
 deleteAll.addEventListener('click', function () {
     let element = document.querySelectorAll('.checked')
     element.forEach(function (item) {
@@ -38,5 +50,3 @@ deleteAll.addEventListener('click', function () {
     })
     deleteButton();
 })
-
-
